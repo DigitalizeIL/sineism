6a901ccd8225530fd20dba9b537b5075700a8b80
Neriya Rosner <neri.coder@gmail.com>
2023-09-08 17:41:08 +0300

    create header link

--- app/_core/components/Layout/Header/Header.tsx
+++ app/_core/components/Layout/Header/Header.tsx
@@ -1,14 +1,11 @@
 import React from "react"
-import Link from "next/link"
-import clsx from "clsx"
 import AuthStatus from "@/app/(authentication)/components/AuthStatus"
 import { Logo } from "@/components/Logo"
 import { categoriesService } from "@/app/(posts)/(modules)/categories/lib/services/CategoriesService"
 import { LINKS } from "@/components/Layout/Header/consts"
+import { HeaderLink } from "@/components/Layout/Header/HeaderLink"
 
 export const Header = async () => {
-    const pathname = 1
-
     const categories = await categoriesService.getAllCategories()
 
     return (
@@ -30,7 +27,6 @@ export const Header = async () => {
                     <HeaderLink
                         key={link.id}
                         href={`/categories/${link.id}`}
-                        isActive={pathname === link.id}
                         label={link.name}
                         className={"h-full p-2"}
                     />
@@ -40,26 +36,5 @@ export const Header = async () => {
                 <AuthStatus />
             </div>
         </div>
-    )
-}
-
-const HeaderLink = (props: {
-    href: string
-    label: string
-    className?: string
-    isActive?: boolean
-}) => {
-    return (
-        <Link
-            className={clsx([
-                props.isActive && "border-b-2 border-b-secondary",
-                "hover:border-b-secondary hover:border-b-2",
-                "h-full p-2 flex items-center justify-center",
-                "text-xs",
-                props.className,
-            ])}
-            href={props.href}>
-            {props.label}
-        </Link>
     )
 }
---
+++ app/_core/components/Layout/Header/HeaderLink.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import Link from "next/link"
+import clsx from "clsx"
+import React from "react"
+import { usePathname } from "next/navigation"
+
+export const HeaderLink = (props: {
+    href: string
+    label: string
+    className?: string
+}) => {
+    const pathname = usePathname()
+
+    const isActive = pathname === props.href
+
+    return (
+        <Link
+            className={clsx([
+                isActive && "border-b-2 border-b-secondary",
+                "hover:border-b-secondary hover:border-b-2",
+                "h-full p-2 flex items-center justify-center",
+                "text-xs",
+                props.className,
+            ])}
+            href={props.href}>
+            {props.label}
+        </Link>
+    )
+}
