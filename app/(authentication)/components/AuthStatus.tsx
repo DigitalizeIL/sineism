da3665b689ee2db2b9b8ba635eafe93ed9a86d26
Neriya Rosner <neri.coder@gmail.com>
2023-09-08 15:41:17 +0300

    display username

---
+++ app/(authentication)/components/AuthStatus.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { signOut, useSession } from "next-auth/react"
+import { TEXTS } from "@/app/(authentication)/components/consts"
+import { useEffect } from "react"
+import { redirect } from "next/navigation"
+
+export default function AuthStatus() {
+    const session = useSession()
+
+    useEffect(() => {
+        if (!session) redirect("/")
+    }, [session])
+
+    return (
+        <div className={"flex items-center"}>
+            {session?.data?.user?.name ? (
+                <>
+                    <span className={"me-2 text-slate-500"}>
+                        {TEXTS.usernamePrefix} {session.data.user.name}
+                    </span>
+                    <span className={"me-2 text-slate-500"}>-</span>
+                </>
+            ) : null}
+            <button
+                className="text-slate-400 hover:text-slate-200 transition-all"
+                onClick={() => signOut({ callbackUrl: "/" })}>
+                {TEXTS.signOut}
+            </button>
+        </div>
+    )
+}
--- app/(authentication)/components/consts.ts
+++ app/(authentication)/components/consts.ts
@@ -1,4 +1,5 @@
 export const TEXTS = {
     signedInAs: "Signed in as",
     signOut: "Sign me out",
+    usernamePrefix: "Welcome,",
 }
