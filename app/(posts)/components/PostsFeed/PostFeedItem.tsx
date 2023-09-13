f7fe9a27f085e8efbea7746e6046718e1c57ea43
Neriya Rosner <neri.coder@gmail.com>
2023-09-13 19:24:57 +0300

    get active category

--- app/(posts)/components/PostsFeed/PostFeedItem.tsx
+++ app/(posts)/components/PostsFeed/PostFeedItem.tsx
@@ -6,6 +6,7 @@ import { ModalWithButton } from "@/components/Modal"
 import { IPost } from "@/app/(posts)/lib/interfaces/IPost"
 import { SaveBookmarkButton } from "@/app/(posts)/(modules)/bookmark/components/SaveBookmarkButton"
 import { getAppServerSession } from "@/app/(authentication)/lib/utils/session"
+import { bookmarkService } from "@/app/(posts)/(modules)/bookmark/lib/services/BookmarkService"
 
 type PostFeedItemProps = {
     post: IPost
@@ -15,6 +16,15 @@ type PostFeedItemProps = {
 export const PostFeedItem = async ({ post }: PostFeedItemProps) => {
     const session = await getAppServerSession()
 
+    let activeBookmark
+
+    if (session?.user) {
+        activeBookmark = await bookmarkService.getBookmarkByUserAndCategory(
+            session.user.id,
+            post.categoryId
+        )
+    }
+
     return (
         <Card
             className={"w-full max-w-lg flex-col"}
@@ -23,8 +33,10 @@ export const PostFeedItem = async ({ post }: PostFeedItemProps) => {
             actions={[
                 <SaveBookmarkButton
                     key={"bookmark"}
+                    categoryId={post.categoryId}
                     postId={post.id}
                     page={post.id}
+                    isActive={activeBookmark?.postId === post.id}
                 />,
                 ...(session?.user?.role === USER_ROLES.admin
                     ? [
