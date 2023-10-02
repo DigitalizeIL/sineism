a85deb37411c2a3e4914b2ebf83c75608cacb146
Neriya Rosner <neri.coder@gmail.com>
2023-10-02 20:08:16 +0300

    use comments modal

--- app/(protected)/(payment)/(modules)/comments/components/CommentWithPaymentContainer.tsx
+++ app/(protected)/(payment)/(modules)/comments/components/CommentWithPaymentContainer.tsx
@@ -1,21 +1,25 @@
 import { PaymentModal } from "@/app/(protected)/(payment)/(modules)/comments/components/PaymentModal"
-import { CreateCommentForm } from "@/app/(protected)/(posts)/(modules)/comments/components/CreateCommentForm"
 import { quotaService } from "@/app/(protected)/(payment)/(modules)/comments/lib/QuotaService"
 import { getAppServerSession } from "@/app/(authentication)/lib/utils/session"
+import { CommentsModal } from "@/app/(protected)/(payment)/(modules)/comments/components/CommentsModal"
+import { CreateCommentForm } from "@/app/(protected)/(posts)/(modules)/comments/components/CreateCommentForm"
 
 export const CommentWithPaymentContainer = async () => {
     const session = await getAppServerSession()
     if (!session?.user) return
 
     const quota = await quotaService.getQuota(session.user.id)
+    const shouldPay = !quota || quota.quota <= 0
 
     return (
         <div>
             <div className={"flex items-center justify-end"}>
-                {!quota || quota.quota <= 0 ? (
+                {shouldPay ? (
                     <PaymentModal />
                 ) : (
-                    <CreateCommentForm />
+                    <CommentsModal>
+                        <CreateCommentForm />
+                    </CommentsModal>
                 )}
             </div>
         </div>
