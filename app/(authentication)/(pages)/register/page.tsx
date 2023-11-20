881d5aa7e066d8dccc39d9615be134c3d4cf43fe
Neriya Rosner <neri.coder@gmail.com>
2023-11-20 21:25:35 +0200

    auth background image

--- app/(authentication)/(pages)/register/page.tsx
+++ app/(authentication)/(pages)/register/page.tsx
@@ -4,12 +4,12 @@ import { Logo } from "@/components/Logo"
 export default function RegisterPage() {
     return (
         <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
-            <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
-                <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+            <div className="backdrop-blur bg-login-image bg-no-repeat bg-center z-10 w-full max-w-lg overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+                <div className="flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16">
                     <Logo />
 
                     <h3 className="text-xl font-semibold">Sign Up</h3>
-                    <p className="text-sm text-gray-500">
+                    <p className="text-sm">
                         Create an account with your email and password
                     </p>
                 </div>
