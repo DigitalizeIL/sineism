050d1833c6b762e70f00b4f3662490915aa98462
Neriya Rosner <neri.coder@gmail.com>
2023-11-20 20:55:25 +0200

    use next request

--- app/(authentication)/api/register/route.ts
+++ app/(authentication)/api/register/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { authenticationService } from "@/app/(authentication)/lib/services/AuthenticationService"
 import { UserAlreadyExists } from "@/app/(authentication)/lib/errors/UserAlreadyExists"
 import { RegisterArgs } from "@/app/(authentication)/lib/types/AuthenticationTypes"
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     const registerData: RegisterArgs = await req.json()
 
     try {
