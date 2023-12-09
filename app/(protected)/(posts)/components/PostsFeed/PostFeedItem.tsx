96d7c9be82b37519e74e3a6f7b1186612c37ec62
Neriya Rosner <neri.coder@gmail.com>
2023-12-09 09:43:54 +0200

    design

--- app/(protected)/(posts)/components/PostsFeed/PostFeedItem.tsx
+++ app/(protected)/(posts)/components/PostsFeed/PostFeedItem.tsx
@@ -21,21 +21,22 @@ export const PostFeedItem = async ({ post, page }: PostFeedItemProps) => {
             description={post.content}
             title={
                 <div className="flex flex-row justify-between items-center w-full mb-4">
-                    <div className={"flex flex-row "}>
-                        <SaveBookmarkButton
-                            categoryId={post.categoryId}
-                            postId={post.id}
-                            page={page || 1}
-                        />
+                    <div className="flex items-center text-lg">
+                        {post.postNumber || -1}
                     </div>
+
                     <div className="flex flex-row justify-center w-full">
                         <h3 className="text-lg font-medium text-stone-900 self-center !mb-0">
                             {post.title}
                         </h3>
                     </div>
 
-                    <div className="flex items-center text-sm text-stone-500">
-                        {post.postNumber || -1}
+                    <div className={"flex flex-row "}>
+                        <SaveBookmarkButton
+                            categoryId={post.categoryId}
+                            postId={post.id}
+                            page={page || 1}
+                        />
                     </div>
                     {session?.user?.role === USER_ROLES.admin ? (
                         <>
