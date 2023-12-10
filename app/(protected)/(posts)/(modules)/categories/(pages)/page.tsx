187ec3a930800ada6dd79928d21fc517839b7137
Neriya Rosner <neri.coder@gmail.com>
2023-12-10 08:48:50 +0200

    add comments link

--- app/(protected)/(posts)/(modules)/categories/(pages)/page.tsx
+++ app/(protected)/(posts)/(modules)/categories/(pages)/page.tsx
@@ -2,6 +2,7 @@
 
 import { CATEGORIES } from "@/app/(protected)/(posts)/(modules)/categories/consts/categories"
 import Link from "next/link"
+import { COMMENTS_LINK } from "@/components/Layout/Header/consts"
 
 export default function CategoriesPage() {
     return (
@@ -23,6 +24,20 @@ export default function CategoriesPage() {
                     </Link>
                 </div>
             ))}
+
+            <div className={"flex justify-center items-center"}>
+                <Link
+                    href={COMMENTS_LINK.href}
+                    passHref>
+                    <button
+                        className={
+                            "w-64 h-64 bg-primary rounded-xl shadow-lg m-4 flex justify-center items-center cursor-pointer" +
+                            "hover:bg-primary-600 hover:shadow-xl"
+                        }>
+                        {COMMENTS_LINK.label}
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
