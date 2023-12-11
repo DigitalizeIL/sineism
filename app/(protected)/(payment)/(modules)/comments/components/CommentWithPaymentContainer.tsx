d1fe7cd518e1c94927581a06b3e0b2541abee359
Neriya Rosner <neri.coder@gmail.com>
2023-12-11 09:01:25 +0200

    add quota indicator

--- app/(protected)/(payment)/(modules)/comments/components/CommentWithPaymentContainer.tsx
+++ app/(protected)/(payment)/(modules)/comments/components/CommentWithPaymentContainer.tsx
@@ -2,15 +2,14 @@ import { PaymentModal } from "@/app/(protected)/(payment)/(modules)/comments/com
 import { quotaService } from "@/app/(protected)/(payment)/(modules)/comments/lib/QuotaService"
 import { getAppServerSession } from "@/app/(authentication)/lib/utils/session"
 import { CommentsModal } from "@/app/(protected)/(payment)/(modules)/comments/components/CommentsModal"
-import { CommentForm } from "@/app/(protected)/(posts)/(modules)/comments/components/CommentForm"
 import { CommentFormContainer } from "@/app/(protected)/(posts)/(modules)/comments/components/CommentFormContainer"
 
 export const CommentWithPaymentContainer = async () => {
     const session = await getAppServerSession()
     if (!session?.user) return
 
-    const quota = await quotaService.getQuota(session.user.id)
-    const shouldPay = !quota || quota.quota <= 0
+    const quotaObject = await quotaService.getQuota(session.user.id)
+    const shouldPay = !quotaObject || quotaObject.quota <= 0
 
     return (
         <div>
@@ -20,6 +19,11 @@ export const CommentWithPaymentContainer = async () => {
                 ) : (
                     <CommentsModal>
                         <CommentFormContainer />
+                        <div className={"-mt-2 m-2 flex items-center gap-1"}>
+                            <span>נשאר לך</span>
+                            <span>{quotaObject?.quota}</span>
+                            <span>תגובות</span>
+                        </div>
                     </CommentsModal>
                 )}
             </div>
