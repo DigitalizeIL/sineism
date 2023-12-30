b81de6ee2d5be72196fed215bd79496a396429fb
Neriya Rosner <neriyarosner@gmail.com>
2023-12-30 14:33:06 +0200

    add settings

--- app/(protected)/(posts)/(pages)/management/page.tsx
+++ app/(protected)/(posts)/(pages)/management/page.tsx
@@ -1,8 +1,9 @@
-import { PostsFeed } from "@/app/(protected)/(posts)/components/PostsFeed"
-import { ModalWithButton } from "@/components/Modal"
-import { CreateCategoryForm } from "@/app/(protected)/(posts)/(modules)/categories/components/CreateCategoryForm"
+import {PostsFeed} from "@/app/(protected)/(posts)/components/PostsFeed"
+import {ModalWithButton} from "@/components/Modal"
+import {CreateCategoryForm} from "@/app/(protected)/(posts)/(modules)/categories/components/CreateCategoryForm"
 import React from "react"
-import { PostCreateOrEditFormServer } from "@/app/(protected)/(posts)/components/PostCreateOrEditFormServer"
+import {PostCreateOrEditFormServer} from "@/app/(protected)/(posts)/components/PostCreateOrEditFormServer"
+import {Settings} from "@/app/(protected)/(posts)/(modules)/settings/components/Settings";
 
 export default function PostsPage() {
     return (
@@ -10,13 +11,14 @@ export default function PostsPage() {
             <div className="w-screen h-screen flex flex-col space-y-5 justify-start items-center">
                 <h2 className="text-4xl font-bold">Admin Only Page</h2>
                 <div className="flex justify-center mt-4">
-                    <PostCreateOrEditFormServer />
+                    <PostCreateOrEditFormServer/>
                     <ModalWithButton buttonText={"Create Category"}>
-                        <CreateCategoryForm />
+                        <CreateCategoryForm/>
                     </ModalWithButton>
+                    <Settings/>
                 </div>
                 <h3 className="text-2xl font-bold">All Posts</h3>
-                <PostsFeed />
+                <PostsFeed/>
             </div>
         </div>
     )
