91346f14f3cd2c53b5c0d5608144e195dcf0b547
Neriya Rosner <neri.coder@gmail.com>
2024-01-12 13:23:41 +0200

    add use controller

--- app/_core/components/Form/TextArea/TextArea.tsx
+++ app/_core/components/Form/TextArea/TextArea.tsx
@@ -1,33 +1,35 @@
 "use client"
 
-import { ChangeEvent } from "react"
+import { FieldValues, useController, UseControllerProps } from "react-hook-form"
 import clsx from "clsx"
 
-export const TextArea = (props: {
-    value?: string
-    defaultValue?: string
-    name?: string
-    placeholder?: string
-    className?: string
-    onChange?: (value: string, event: ChangeEvent<HTMLTextAreaElement>) => void
-    rows?: number
-}) => {
+export function TextArea<Dto extends FieldValues>(
+    props: {
+        placeholder?: string
+        className?: string
+        rows?: number
+    } & UseControllerProps<Dto>
+) {
+    const { field, fieldState } = useController(props)
+
     return (
-        <textarea
-            defaultValue={props.defaultValue}
-            name={props.name}
-            rows={props.rows || 3}
-            className={clsx([
-                "border-2 border-gray-300 rounded-md p-2",
-                "focus:outline-none",
-                "focus:ring-2",
-                "focus:ring-blue-400",
-                "focus:border-transparent",
-                props.className,
-            ])}
-            placeholder={props.placeholder}
-            value={props.value}
-            onChange={(e) => props.onChange?.(e.target.value, e)}
-        />
+        <div>
+            <textarea
+                rows={props.rows || 3}
+                placeholder={props.placeholder}
+                className={clsx([
+                    "border-2 border-gray-300 rounded-md p-2",
+                    "focus:outline-none",
+                    "focus:ring-2",
+                    "focus:border-transparent",
+                    fieldState.invalid
+                        ? "focus:ring-red-400"
+                        : "focus:ring-blue-400",
+                    props.className,
+                ])}
+                {...field}
+            />
+            <div>{fieldState.error?.message}</div>
+        </div>
     )
 }
