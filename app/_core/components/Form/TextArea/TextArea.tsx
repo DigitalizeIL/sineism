819caf019103e0477fa089b69baa139f1a398e24
Neriya Rosner <neri.coder@gmail.com>
2024-01-12 14:16:24 +0200

    turn clean

--- app/_core/components/Form/TextArea/TextArea.tsx
+++ app/_core/components/Form/TextArea/TextArea.tsx
@@ -1,35 +1,32 @@
 "use client"
 
-import { FieldValues, useController, UseControllerProps } from "react-hook-form"
 import clsx from "clsx"
+import { ChangeEventHandler } from "react"
 
-export function TextArea<Dto extends FieldValues>(
-    props: {
-        placeholder?: string
-        className?: string
-        rows?: number
-    } & UseControllerProps<Dto>
-) {
-    const { field, fieldState } = useController(props)
+export type TextAreaProps = {
+    placeholder?: string
+    className?: string
+    rows?: number
+    value: string
+    isInvalid?: boolean
+    onChange?: ChangeEventHandler<HTMLTextAreaElement>
+}
 
+export function TextArea(props: TextAreaProps) {
     return (
-        <div>
-            <textarea
-                rows={props.rows || 3}
-                placeholder={props.placeholder}
-                className={clsx([
-                    "border-2 border-gray-300 rounded-md p-2",
-                    "focus:outline-none",
-                    "focus:ring-2",
-                    "focus:border-transparent",
-                    fieldState.invalid
-                        ? "focus:ring-red-400"
-                        : "focus:ring-blue-400",
-                    props.className,
-                ])}
-                {...field}
-            />
-            <div>{fieldState.error?.message}</div>
-        </div>
+        <textarea
+            onChange={props.onChange}
+            value={props.value}
+            rows={props.rows || 3}
+            placeholder={props.placeholder}
+            className={clsx([
+                "border-2 border-gray-300 rounded-md p-2",
+                "focus:outline-none",
+                "focus:ring-2",
+                "focus:border-transparent",
+                props.isInvalid ? "focus:ring-red-400" : "focus:ring-blue-400",
+                props.className,
+            ])}
+        />
     )
 }
