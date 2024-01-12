5f8da25af6567f84c744bbcdc6c872fd6963f8d3
Neriya Rosner <neri.coder@gmail.com>
2024-01-12 16:20:38 +0200

    handle user exists

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,31 @@
-import {CATEGORIES} from "../app/(protected)/(posts)/(modules)/categories/consts/categories"
+import { CATEGORIES } from "../app/(protected)/(posts)/(modules)/categories/consts/categories"
 import prisma from "../app/_core/lib/prisma"
-import {hash} from "bcryptjs";
+import { hash } from "bcryptjs"
 
 async function createUsers() {
-    const admin = await prisma.user.create({
-        data: {
-            name: "Neriya Rosner",
-            email: "neri.coder@gmail.com",
-            role: "ADMIN",
-            password: await hash("DevelopThatStuff", 10),
-        },
-    })
-    console.log(`Created admin with id: ${admin.id}`)
+    try {
+        const admin = await prisma.user.create({
+            data: {
+                name: "Neriya Rosner",
+                email: "neri.coder@gmail.com",
+                role: "ADMIN",
+                password: await hash("DevelopThatStuff", 10),
+            },
+        })
+
+        console.log(`Created admin with id: ${admin.id}`)
+    } catch (e: any) {
+        if (e.code === "P2002") {
+            return console.log("User already exists")
+        } else {
+            throw e
+        }
+    }
 }
 
 async function createCategories() {
     const categories = await prisma.category.createMany({
-        data: CATEGORIES.map(category => ({
+        data: CATEGORIES.map((category) => ({
             name: category.name,
         })),
     })
@@ -26,16 +35,20 @@ async function createCategories() {
 
 async function createSettings() {
     const settings = await prisma.settings.createMany({
-        data: [{
-            key: "posts_per_page",
-            value: "10",
-        }, {
-            key: "comments_cost_usd",
-            value: "5",
-        }, {
-            key: "comments_amount_per_purchase",
-            value: "3",
-        }]
+        data: [
+            {
+                key: "posts_per_page",
+                value: "10",
+            },
+            {
+                key: "comments_cost_usd",
+                value: "5",
+            },
+            {
+                key: "comments_amount_per_purchase",
+                value: "3",
+            },
+        ],
     })
 
     console.log(`Created ${settings.count} settings`)
