ba303f26efa644699c9d55daf03576b5bc6a9879
Neriya Rosner <neri.coder@gmail.com>
2024-01-27 11:17:23 +0200

    add react-select

--- app/_core/components/Form/Select/Select.tsx
+++ app/_core/components/Form/Select/Select.tsx
@@ -1,34 +1,60 @@
 "use client"
 
 import clsx from "clsx"
-import { ChangeEvent, useEffect, useMemo, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
+import Select2 from "react-select"
 
-export const Select = (props: {
+type Option = {
+    label: string
+    value: string | number
+}
+type SelectProps = {
     name: string
     className?: string
     defaultValue?: string | number
     value?: string | number
     placeholder?: string
     onChange?: (value: string | number) => void
-    options: { label: string; value: string | number | undefined }[]
-}) => {
-    const selectedOption = useMemo(() => {
-        return props.options.find(
-            (option) => option.value === props.defaultValue
-        )
-    }, [props.defaultValue, props.options])
+    isMulti?: boolean
+    options: Option[]
+}
 
+export const Select = (props: SelectProps) => {
     const [value, setValue] = useState(props.value)
 
     useEffect(() => {
         setValue(props.value)
     }, [props.value])
 
+    const options: readonly Option[] = props.options
+
     const change = (e: ChangeEvent<HTMLSelectElement>) => {
         setValue(e.target.value)
         props.onChange?.(e.target.value)
     }
 
+    if (props.isMulti) {
+        return (
+            <Select2
+                isMulti
+                isSearchable
+                options={props.options}
+                placeholder={props.placeholder}
+                name={props.name}
+                styles={{
+                    option: (base, props) => ({
+                        ...base,
+                        textAlign: "right",
+                        // paddingInlineStart: "10px",
+                    }),
+                    placeholder: (base, props) => ({
+                        ...base,
+                        textAlign: "right",
+                    }),
+                }}
+            />
+        )
+    }
     return (
         <select
             value={value || "empty"}
