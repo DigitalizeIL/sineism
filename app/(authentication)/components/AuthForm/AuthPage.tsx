ff425bf896cff634f824579d28ae3eebb0bc77d8
Neriya Rosner <neri.coder@gmail.com>
2024-02-23 09:17:24 +0200

    split to components

---
+++ app/(authentication)/components/AuthForm/AuthPage.tsx
@@ -0,0 +1,26 @@
+import {
+    SignInForm,
+    SignUpForm,
+} from "@/app/(authentication)/components/AuthForm"
+
+import { Logo } from "@/components/Logo"
+import { ReactJSXElementChildrenAttribute } from "@emotion/react/types/jsx-namespace"
+import { ReactNode } from "react"
+
+export const AuthPage = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+            <div className="backdrop-blur bg-login-image bg-no-repeat bg-center z-10 w-full max-w-lg overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+                <div className="flex flex-col items-center justify-center space-y-3 px-4 py-6 pt-8 text-center sm:px-16">
+                    <Logo />
+
+                    <h3 className="text-xl font-semibold">Sign Up</h3>
+                    <p className="text-sm">
+                        Create an account with your email and password
+                    </p>
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}
---
+++ app/(authentication)/components/AuthForm/SignInForm.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import {
+    LOGIN_URL,
+    REGISTER_URL,
+    TEXTS,
+} from "@/app/(authentication)/components/AuthForm/consts"
+
+import { Button } from "@/components/Button"
+import { FormEvent } from "react"
+import Link from "next/link"
+import { useAuthForm } from "@/app/(authentication)/components/AuthForm/useAuthForm"
+
+export const SignInForm = () => {
+    const { loading, signUp, signIn, errorHandler } = useAuthForm()
+
+    const submitForm = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const data = e.currentTarget
+
+        if (!data) {
+            return errorHandler("Something went wrong!")
+        }
+
+        await signIn({
+            email: data.email.value,
+            password: data.password.value,
+        })
+    }
+
+    return (
+        <form
+            onSubmit={submitForm}
+            className="flex flex-col space-y-4 px-4 py-8 sm:px-16">
+            <div>
+                <label
+                    htmlFor="email"
+                    className="block text-xs uppercase">
+                    {TEXTS.fields.email}
+                </label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="email@gmail.com"
+                    autoComplete="email"
+                    required
+                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                />
+            </div>
+            <div>
+                <label
+                    htmlFor="password"
+                    className="block text-xs  uppercase">
+                    {TEXTS.fields.password}
+                </label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                />
+            </div>
+            <Button
+                loading={loading}
+                className={"text-white"}>
+                <p>{TEXTS.signIn}</p>
+            </Button>
+            <p className="text-center text-sm text-white">
+                {TEXTS.noAccount}
+                <Link
+                    href={REGISTER_URL}
+                    className="font-semibold text-white pl-1">
+                    {TEXTS.signUp}
+                </Link>{" "}
+                {TEXTS.forFree}
+            </p>
+        </form>
+    )
+}
