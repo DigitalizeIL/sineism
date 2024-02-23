ade3a1f6987fa3a68c189420e40c93e863b32d8e
Neriya Rosner <neri.coder@gmail.com>
2024-02-23 10:17:14 +0200

    try

--- app/(protected)/(payment)/(modules)/comments/actions/CreateOrder.ts
+++ app/(protected)/(payment)/(modules)/comments/actions/CreateOrder.ts
@@ -1,9 +1,9 @@
 "use server"
 
-import { quotaService } from "@/app/(protected)/(payment)/(modules)/comments/lib/QuotaService"
-import { orderService } from "@/app/(protected)/(payment)/lib/OrderService"
-import { settingsService } from "@/app/(protected)/(posts)/(modules)/settings/lib/services/SettingsService"
 import { SettingKey } from "@/app/(protected)/(posts)/(modules)/settings/lib/interfaces/ISettings"
+import { orderService } from "@/app/(protected)/(payment)/lib/OrderService"
+import { quotaService } from "@/app/(protected)/(payment)/(modules)/comments/lib/QuotaService"
+import { settingsService } from "@/app/(protected)/(posts)/(modules)/settings/lib/services/SettingsService"
 
 export const createOrder = async (
     order: {
@@ -12,29 +12,37 @@ export const createOrder = async (
     },
     userId: number
 ) => {
-    const price = await settingsService.getSettingByKey(
-        SettingKey.comments_cost_usd
-    )
-    const amount = await settingsService.getSettingByKey(
-        SettingKey.comments_amount_per_purchase
-    )
+    try {
+        const price = await settingsService.getSettingByKey(
+            SettingKey.comments_cost_usd
+        )
+        const amount = await settingsService.getSettingByKey(
+            SettingKey.comments_amount_per_purchase
+        )
 
-    if (price?.value === undefined) {
-        throw Error("No price")
+        if (price?.value === undefined) {
+            throw Error("No price")
+        }
+
+        if (amount?.value === undefined) {
+            throw Error("No amount")
+        }
+
+        console.log("createOrder", order)
+
+        await orderService.createOrder({
+            ...order,
+            price: Number(price?.value),
+            userId,
+        })
+        await quotaService.addQuota(userId, Number(amount.value))
+
+        console.log("DONEEE", order)
+
+        return true
+    } catch (e) {
+        console.error(e)
+
+        return false
     }
-
-    if (amount?.value === undefined) {
-        throw Error("No amount")
-    }
-
-    console.log("createOrder", order)
-
-    await orderService.createOrder({
-        ...order,
-        price: Number(price?.value),
-        userId,
-    })
-    await quotaService.addQuota(userId, Number(amount.value))
-
-    console.log("DONEEE", order)
 }
