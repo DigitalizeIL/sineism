5908ff69ed301d36e29b1412b2e35609aed527c2
Neriya Rosner <neri.coder@gmail.com>
2024-03-09 15:05:28 +0200

    suspense

--- app/(protected)/(posts)/(modules)/categories/components/CategoryFeed.tsx
+++ app/(protected)/(posts)/(modules)/categories/components/CategoryFeed.tsx
@@ -1,6 +1,7 @@
 import { CategoryHeader } from "@/app/(protected)/(posts)/(modules)/categories/components/CategoryHeader"
 import { ICategory } from "@/app/(protected)/(posts)/(modules)/categories/lib/interfaces/ICategory"
 import { PostFeedItem } from "@/app/(protected)/(posts)/components/PostsFeed/PostFeedItem"
+import { PostFeedItemSkelleton } from "../../../components/PostsFeed/PostFeedItemSkeleton"
 import { Suspense } from "react"
 
 type CategoryFeedProps = {
@@ -19,12 +20,11 @@ export function CategoryFeed({ category, page }: CategoryFeedProps) {
             </Suspense>
             <div className={"flex flex-col justify-center items-center w-full"}>
                 {category.posts?.map((post) => (
-                    <Suspense key={post.id}>
-                        <PostFeedItem
-                            page={page}
-                            post={post}
-                        />
-                    </Suspense>
+                    <PostFeedItem
+                        key={post.id}
+                        page={page}
+                        post={post}
+                    />
                 ))}
             </div>
         </>
--- app/(protected)/(posts)/components/PostsFeed/PostFeedItem.tsx
+++ app/(protected)/(posts)/components/PostsFeed/PostFeedItem.tsx
@@ -1,9 +1,11 @@
+import React, { Suspense } from "react"
+
 import { Card } from "@/components/Card"
 import { DeletePostButton } from "@/app/(protected)/(posts)/components/DeletePostButton"
 import { IPost } from "@/app/(protected)/(posts)/lib/interfaces/IPost"
+import { LoadingDots } from "@/app/_core/components/LoadingDots"
 import { PostCreateOrEditFormServer } from "@/app/(protected)/(posts)/components/PostCreateOrEditFormServer"
 import { RatingContainer } from "@/app/(protected)/(posts)/(modules)/rating/components/RatingContainer"
-import React from "react"
 import { SaveBookmarkButtonContainer } from "@/app/(protected)/(posts)/(modules)/bookmark/components/SaveBookmarkButtonContainer"
 import { USER_ROLES } from "@/app/(authentication)/lib/models/UserRole"
 import { getAppServerSession } from "@/app/(authentication)/lib/utils/session"
@@ -35,25 +37,30 @@ export const PostFeedItem = async ({ post, page }: PostFeedItemProps) => {
                     </div>
 
                     <div className={"flex flex-row"}>
-                        {session?.user?.id && (
-                            <SaveBookmarkButtonContainer
-                                pathForRevalidation={`/categories/${post.categoryId}`}
-                                ids={{
-                                    referenceType: post.categoryId.toString(),
-                                    userId: session.user.id,
-                                }}
-                                itemIdToBookmark={post.id.toString()}
-                                page={page || 1}
-                            />
-                        )}
+                        <Suspense fallback={<LoadingDots />}>
+                            {session?.user?.id && (
+                                <SaveBookmarkButtonContainer
+                                    pathForRevalidation={`/categories/${post.categoryId}`}
+                                    ids={{
+                                        referenceType:
+                                            post.categoryId.toString(),
+                                        userId: session.user.id,
+                                    }}
+                                    itemIdToBookmark={post.id.toString()}
+                                    page={page || 1}
+                                />
+                            )}
+                        </Suspense>
                     </div>
-                    {session?.user?.role === USER_ROLES.admin ? (
-                        <>
-                            <DeletePostButton postId={post.id} />
-                            <PostCreateOrEditFormServer post={post} />
-                        </>
-                    ) : null}
-                    <RatingContainer postId={post.id} />
+                    <Suspense>
+                        {session?.user?.role === USER_ROLES.admin ? (
+                            <>
+                                <DeletePostButton postId={post.id} />
+                                <PostCreateOrEditFormServer post={post} />
+                            </>
+                        ) : null}
+                        <RatingContainer postId={post.id} />
+                    </Suspense>
                 </div>
             }
         />
