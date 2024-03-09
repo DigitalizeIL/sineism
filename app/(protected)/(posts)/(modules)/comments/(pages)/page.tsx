e9f567a77dc07e7e0305b1e060da19bf3d1a2d14
Neriya Rosner <neri.coder@gmail.com>
2024-03-09 15:11:20 +0200

    suspense comments

--- app/(protected)/(posts)/(modules)/comments/(pages)/page.tsx
+++ app/(protected)/(posts)/(modules)/comments/(pages)/page.tsx
@@ -1,9 +1,6 @@
-import { commentsService } from "@/app/(protected)/(posts)/(modules)/comments/lib/services/CommentsService"
-import { Comment } from "@/app/(protected)/(posts)/(modules)/comments/components/Comment"
 import { CommentHeader } from "@/app/(protected)/(posts)/(modules)/comments/components/CommentHeader"
-import { settingsService } from "@/app/(protected)/(posts)/(modules)/settings/lib/services/SettingsService"
-import { SettingKey } from "@/app/(protected)/(posts)/(modules)/settings/lib/interfaces/ISettings"
-import { DEFAULT_PAGE_SIZE } from "@/app/(protected)/(posts)/(modules)/categories/consts/pagination"
+import { CommentsFeed } from "../components/CommentsFeed"
+import { Suspense } from "react"
 
 type PageProps = {
     searchParams?: { [key: string]: string }
@@ -11,34 +8,15 @@ type PageProps = {
 
 export default async function CommentsPage(props: PageProps) {
     const page = Number(props.searchParams?.page || 1)
-    const postsPerPage = await settingsService.getSettingByKey(
-        SettingKey.posts_per_page
-    )
-
-    const comments = await commentsService.getAllComments({
-        pagination: {
-            page,
-            perPage: postsPerPage?.value
-                ? Number(postsPerPage?.value)
-                : DEFAULT_PAGE_SIZE,
-        },
-    })
 
     return (
         <div className={"flex justify-center flex-col"}>
-            <CommentHeader page={page} />
-            <div
-                className={
-                    "flex justify-center items-center flex-col overflow-scroll h-auto"
-                }>
-                {comments.map((comment) => (
-                    <Comment
-                        page={page}
-                        comment={comment}
-                        key={comment.id}
-                    />
-                )) || "No Comments"}
-            </div>
+            <Suspense>
+                <CommentHeader page={page} />
+            </Suspense>
+            <Suspense>
+                <CommentsFeed page={page} />
+            </Suspense>
         </div>
     )
 }
---
+++ app/(protected)/(posts)/(modules)/comments/components/CommentsFeed.tsx
@@ -0,0 +1,40 @@
+import { Comment } from "@/app/(protected)/(posts)/(modules)/comments/components/Comment"
+import { DEFAULT_PAGE_SIZE } from "@/app/(protected)/(posts)/(modules)/categories/consts/pagination"
+import { SettingKey } from "@/app/(protected)/(posts)/(modules)/settings/lib/interfaces/ISettings"
+import { commentsService } from "@/app/(protected)/(posts)/(modules)/comments/lib/services/CommentsService"
+import { settingsService } from "@/app/(protected)/(posts)/(modules)/settings/lib/services/SettingsService"
+
+type PageProps = {
+    page: number
+}
+
+export async function CommentsFeed(props: PageProps) {
+    const postsPerPageValue = await settingsService.getSettingByKey(
+        SettingKey.posts_per_page
+    )
+    const postsPerPage = postsPerPageValue?.value
+        ? Number(postsPerPageValue?.value)
+        : DEFAULT_PAGE_SIZE
+
+    const comments = await commentsService.getAllComments({
+        pagination: {
+            page: props.page,
+            perPage: postsPerPage,
+        },
+    })
+
+    return (
+        <div
+            className={
+                "flex justify-center items-center flex-col overflow-scroll h-auto"
+            }>
+            {comments.map((comment) => (
+                <Comment
+                    page={props.page}
+                    comment={comment}
+                    key={comment.id}
+                />
+            ))}
+        </div>
+    )
+}
