1c5aec645fdb71c67656581d1a224a0be8128367
Neriya Rosner <neriyarosner@gmail.com>
2024-04-29 11:32:25 +0300

    use post numbers

--- app/_core/consts/pagination.consts.ts
+++ app/_core/consts/pagination.consts.ts
@@ -3,6 +3,6 @@ import { IPost } from "@/app/(protected)/(posts)/lib/post.interface"
 
 export const COMMENTS_PROPERTY_FOR_CURSOR: keyof Pick<IComment, "id"> = "id"
 export const POST_PROPERTY_FOR_CURSOR: keyof Pick<IPost, "id" | "postNumber"> =
-    "id"
+    "postNumber"
 
 export const PAGINATION_URL_PARAM_KEY = "id"
