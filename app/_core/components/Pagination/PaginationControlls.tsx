8749820c6991001f8738f73e1761d0c57ba35ffc
Neriya Rosner <neriyarosner@gmail.com>
2024-05-05 22:04:40 +0300

    differentiate index and cursor

--- app/_core/components/Pagination/PaginationControlls.tsx
+++ app/_core/components/Pagination/PaginationControlls.tsx
@@ -5,43 +5,75 @@ import { FcNext, FcPrevious } from "react-icons/fc"
 import { Button } from "../Button"
 import { TEXTS } from "./pagination.texts"
 import { useContent } from "../../views/ContentFeed"
-import { useRouter } from "next/navigation"
-import { PAGINATION_URL_PARAM_KEY } from "../../consts/pagination.consts"
+import { useEffect, useMemo } from "react"
+
+const DEFAULT_NUMBER = 0
 
 export const PaginationControlles = () => {
-    const { cursor: page = 0, pageSize, items, updatePage } = useContent()
+    const {
+        cursor = DEFAULT_NUMBER,
+        pageSize,
+        feedItems: items,
+        updateCursor,
+    } = useContent()
 
-    const router = useRouter()
+    const first = items[0]?.cursor
+    const last = items[items.length - 1]?.cursor
 
-    const first = items[0]?.cursor || 0
-    const last = items[items.length - 1]?.cursor || 0
-    const next = items[page + (pageSize - 1)]?.cursor || 0
-    const previous = items[page - (pageSize + 1)]?.cursor || 0
+    const indexOfCursor = useMemo(
+        () => items.findIndex((item) => item.cursor === cursor),
+        [cursor, items]
+    )
 
-    const isLastPage = page === last
-    const isFirstPage = !page || page === first
+    const next = useMemo(() => {
+        if (indexOfCursor === -1) {
+            return items[pageSize]?.cursor || last
+        }
+
+        const offset = indexOfCursor + pageSize
+
+        return items[offset]?.cursor || last
+    }, [indexOfCursor, pageSize, items, last])
+
+    const previous = useMemo(() => {
+        if (indexOfCursor === -1) {
+            return first
+        }
+
+        const offset = indexOfCursor - pageSize
+
+        return items[offset]?.cursor || first
+    }, [indexOfCursor, pageSize, items, first])
+
+    const isLastPage = useMemo(() => {
+        return (
+            cursor === last ||
+            items.slice(-pageSize).find((item) => item.cursor === cursor)
+        )
+    }, [cursor, items, last, pageSize])
+
+    const isFirstPage = !cursor || cursor === first
 
     const goToNextPage = () => {
-        router.push(`?${PAGINATION_URL_PARAM_KEY}=${next || last}`)
-        updatePage(next || last)
+        updateCursor(next)
     }
+
     const goToPrevPage = () => {
-        router.push(`?${PAGINATION_URL_PARAM_KEY}=${previous || first}`)
-        updatePage(previous || first)
+        updateCursor(previous)
     }
 
     return (
         <div className={"flex items-center justify-center px-4 gap-2"}>
             <Button
                 onClick={goToPrevPage}
-                isDisabled={isFirstPage}
+                isDisabled={!!isFirstPage}
                 type={"ghost"}
                 className="bg-blue-500 hover:bg-blue-600">
                 <FcNext /> {TEXTS.previousPage}
             </Button>
             <Button
                 onClick={goToNextPage}
-                isDisabled={isLastPage}
+                isDisabled={!!isLastPage}
                 type={"ghost"}
                 className="bg-blue-500 hover:bg-blue-600">
                 {TEXTS.nextPage} <FcPrevious />
