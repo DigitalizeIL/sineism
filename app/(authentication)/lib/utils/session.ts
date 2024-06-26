298a90662fab42e4dff5ff185e5b8c0642d034e1
Neriya Rosner <neri.coder@gmail.com>
2024-06-26 20:01:23 +0300

    type

--- app/(authentication)/lib/utils/session.ts
+++ app/(authentication)/lib/utils/session.ts
@@ -1,6 +1,21 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/app/(authentication)/lib/AuthOptions"
+import { Session } from "next-auth"
 
-export const getAppServerSession = () => {
-    return getServerSession(authOptions)
+type NonNullableSession = Session & { user: NonNullable<Session["user"]> }
+
+export const getAppServerSession = async <T extends boolean>(
+    userHasToBeLoggedIn?: T
+): Promise<T extends true ? NonNullableSession : Session | null> => {
+    const session = await getServerSession(authOptions)
+
+    if (userHasToBeLoggedIn) {
+        if (!session?.user) {
+            throw new Error("User must be logged in")
+        }
+
+        return session as NonNullableSession
+    }
+
+    return session as any
 }
