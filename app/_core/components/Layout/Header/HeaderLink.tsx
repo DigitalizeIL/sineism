82e9217b4ddffb25acea5b9f2256b128f0f4313a
Neriya Rosner <neri.coder@gmail.com>
2024-06-26 18:31:07 +0300

    no prefetch

--- app/_core/components/Layout/Header/HeaderLink.tsx
+++ app/_core/components/Layout/Header/HeaderLink.tsx
@@ -26,7 +26,6 @@ export const HeaderLink = (props: {
                 "text-center",
                 props.className,
             ])}
-            prefetch={true}
             href={props.href}>
             {props.label}
             {props.icon ? (
