a958f693ce2fc1d3af23a38989b209dff969dd43
Neriya Rosner <neri.coder@gmail.com>
2024-07-05 14:39:24 +0300

    extract action delete

--- app/(protected)/(posts)/components/DeletePostButton.tsx
+++ app/(protected)/(posts)/components/DeletePostButton.tsx
@@ -1,25 +1,17 @@
+"use client"
+
 import { AiOutlineDelete } from "react-icons/ai"
 import { Button } from "@/components/Button"
-import { FiDelete } from "react-icons/fi"
-import { postsService } from "@/app/(protected)/(posts)/lib/posts.service"
-import { revalidatePath } from "next/cache"
+import { deletePost } from "../actions/deletePost.action"
 
 export const DeletePostButton = (props: { postId: number }) => {
-    const deletePost = async () => {
-        "use server"
-        await postsService.deletePost(props.postId)
-        revalidatePath("/posts")
-    }
-
     return (
-        <div className="flex flex-col space-y-2">
-            <form action={deletePost}>
-                <Button
-                    type="ghost"
-                    className={"text-3xl text-black"}>
-                    <AiOutlineDelete />
-                </Button>
-            </form>
-        </div>
+        <form action={deletePost.bind(null, props)}>
+            <Button
+                type="ghost"
+                className={"text-3xl text-black"}>
+                <AiOutlineDelete />
+            </Button>
+        </form>
     )
 }
