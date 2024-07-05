c2295e5f1ab7c3802e13f569fefc88a3824d8551
Neriya Rosner <neri.coder@gmail.com>
2024-07-05 15:36:31 +0300

    default value

--- app/_core/components/Form/Select/Select.tsx
+++ app/_core/components/Form/Select/Select.tsx
@@ -17,35 +17,28 @@ type SelectProps = {
     onChange?: (value: string | number) => void
     isMulti?: boolean
     options: Option[]
+    defaultValue?: Option["value"]
 }
 
 export const Select = (props: SelectProps) => {
-    const [value, setValue] = useState(props.value)
-
-    useEffect(() => {
-        setValue(props.value)
-    }, [props.value])
-
-    const change = (e: ChangeEvent<HTMLSelectElement>) => {
-        setValue(e.target.value)
-        props.onChange?.(e.target.value)
-    }
-
     if (props.isMulti) {
         return (
             <Select2
                 isMulti
                 isSearchable
+                defaultValue={props.options.find(
+                    (option) => option.value === props.defaultValue
+                )}
                 options={props.options}
                 placeholder={props.placeholder}
                 name={props.name}
                 styles={{
-                    option: (base, props) => ({
+                    option: (base) => ({
                         ...base,
                         textAlign: "right",
                         // paddingInlineStart: "10px",
                     }),
-                    placeholder: (base, props) => ({
+                    placeholder: (base) => ({
                         ...base,
                         textAlign: "right",
                     }),
@@ -54,13 +47,36 @@ export const Select = (props: SelectProps) => {
         )
     }
 
+    return <VanilaSelect {...props} />
+}
+
+const VanilaSelect = ({
+    value: _value,
+    defaultValue,
+    options,
+    name,
+    className,
+    placeholder,
+    onChange,
+}: SelectProps) => {
+    const [value, setValue] = useState(_value || defaultValue)
+
+    useEffect(() => {
+        setValue(_value !== undefined ? _value : defaultValue)
+    }, [_value, defaultValue])
+
+    const change = (e: ChangeEvent<HTMLSelectElement>) => {
+        setValue(e.target.value)
+        onChange?.(e.target.value)
+    }
+
     return (
         <select
             value={value || "empty"}
             onChange={change}
-            name={props.name}
+            name={name}
             className={clsx([
-                props.className,
+                className,
                 "w-full border-2 border-gray-300 rounded-md  p-2",
                 "focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent",
                 !value && "text-gray-400",
@@ -68,9 +84,9 @@ export const Select = (props: SelectProps) => {
             <option
                 value={"empty"}
                 disabled>
-                {props.placeholder || "Select"}
+                {placeholder || "Select"}
             </option>
-            {props.options.map((option) => (
+            {options.map((option) => (
                 <option
                     key={option.value}
                     value={option.value}>
