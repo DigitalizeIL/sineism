250955546998c52cb2b289812a75fbfaccaa1676
Neriya Rosner <neri.coder@gmail.com>
2024-07-05 14:17:33 +0300

    fix redirect

--- app/(protected)/(posts)/(modules)/bookmark/components/MoveToBookmarkButton.tsx
+++ app/(protected)/(posts)/(modules)/bookmark/components/MoveToBookmarkButton.tsx
@@ -5,20 +5,19 @@ import { PAGINATION_URL_PARAM_KEY } from "@/app/_core/consts/pagination.consts"
 import { Button } from "@/components/Button"
 import { BiBookBookmark } from "react-icons/bi"
 import Link from "next/link"
+import { useContent } from "@/app/_core/views/ContentFeed"
 
 type ComponentProps = {
     activeBookmark: IBookmark
 }
 export const MoveToBookmarkButton = ({ activeBookmark }: ComponentProps) => {
+    const { updateCursor } = useContent()
     return (
-        <Link
-            passHref
-            href={`?${PAGINATION_URL_PARAM_KEY}=${activeBookmark?.bookmarkedItemId}`}>
-            <Button
-                type={"ghost"}
-                className="bg-blue-500 hover:bg-blue-600">
-                <BiBookBookmark />
-            </Button>
-        </Link>
+        <Button
+            onClick={() => updateCursor(activeBookmark.bookmarkedItemId)}
+            type={"ghost"}
+            className="bg-blue-500 hover:bg-blue-600">
+            <BiBookBookmark />
+        </Button>
     )
 }
